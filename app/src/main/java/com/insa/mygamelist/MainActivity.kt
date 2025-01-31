fdfc621aeebcdbeeb8899fde2ab22d377812e4e3
Eléonore Vanacker <eleonore.vanacker@insa-rennes.fr>
2025-01-31 17:31:04 +0100

    tp

--- app/src/main/java/com/insa/mygamelist/MainActivity.kt
+++ app/src/main/java/com/insa/mygamelist/MainActivity.kt
@@ -4,21 +4,50 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import coil3.compose.AsyncImage
+import com.insa.mygamelist.data.Game
 import com.insa.mygamelist.data.IGDB
 import com.insa.mygamelist.ui.theme.MyGamesListTheme
+import kotlinx.serialization.Serializable
+
+@Serializable
+object GameListe
+@Serializable
+class GameInfo(val game: Game)
 
 @OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +56,7 @@ class MainActivity : ComponentActivity() {
 
         enableEdgeToEdge()
         setContent {
+            val navController = rememberNavController()
 
             MyGamesListTheme {
                 Scaffold(topBar = {
@@ -36,10 +66,66 @@ class MainActivity : ComponentActivity() {
                     ), title = { Text("My Games List") })
                 }, modifier = Modifier.fillMaxSize()) { innerPadding ->
 
-                    Text("À remplir", modifier = Modifier.padding(innerPadding))
+                    NavHost(navController, startDestination = GameListe) {
+                        composable<GameListe> { GameList(Modifier.padding(innerPadding)) }
+                        composable<GameInfo> { GameInfo() }
+                    }
 
+                    }
                 }
             }
         }
     }
-}
+
+    @Composable
+    fun GameList(modifier: Modifier) {
+        LazyColumn(modifier = modifier) {
+            items(IGDB.games.size) { index ->
+                Box(
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(10.dp)
+                            .clip(RoundedCornerShape(16.dp))
+                            .background(Color.LightGray)
+                            .padding(5.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Column(
+                            modifier = Modifier.padding(2.dp),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            AsyncImage(
+                                model = "https:${IGDB.covers.find { it.id == IGDB.games[index].cover }?.url}",
+                                modifier = Modifier.padding(1.dp),
+                                contentDescription = null
+                            )
+                        }
+                        Column(modifier = Modifier.padding(2.dp)) {
+                            Text(
+                                text = IGDB.games[index].name,
+                                fontSize = 20.sp,
+                                fontWeight = FontWeight.W600,
+                                style = TextStyle(textDecoration = TextDecoration.Underline),
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis,
+                                modifier = Modifier.padding(5.dp)
+                            )
+                            Text(text = "Genres : " + IGDB.genres.filter { it.id in IGDB.games[index].genres }
+                                .joinToString(separator = ", ") { it.name },
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis,
+                                modifier = Modifier.padding(2.dp)
+                            )
+                        }
+                    }
+                }
+            }
+        }
+        @Composable
+        fun GameInfo(modifier: Modifier,game: Game){
+            Text(text = game.id.toString(), modifier = Modifier)
+        }
+
+    }
--- app/src/main/java/com/insa/mygamelist/data/IGDB.kt
+++ app/src/main/java/com/insa/mygamelist/data/IGDB.kt
@@ -1,22 +1,39 @@
 package com.insa.mygamelist.data
 
+import android.app.GameState
 import android.content.Context
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.insa.mygamelist.R
+import kotlinx.serialization.Serializable
 
 object IGDB {
 
     lateinit var covers: List<Cover>
+    lateinit var games: List<Game>
+    lateinit var genres: List<Genre>
 
     fun load(context: Context) {
         val coversFromJson: List<Cover> = Gson().fromJson(
             context.resources.openRawResource(R.raw.covers).bufferedReader(),
             object : TypeToken<List<Cover>>() {}.type
         )
+        val gamesFromJson: List<Game> = Gson().fromJson(
+            context.resources.openRawResource(R.raw.games).bufferedReader(),
+            object : TypeToken<List<Game>>() {}.type
+        )
+        val genresFromJson: List<Genre> = Gson().fromJson(
+            context.resources.openRawResource(R.raw.genres).bufferedReader(),
+            object : TypeToken<List<Genre>>() {}.type
+        )
 
         covers = coversFromJson
+        games = gamesFromJson
+        genres = genresFromJson
     }
 }
 
 data class Cover(val id: Long, val url: String)
+data class Genre(val id: Long, val name: String)
+@Serializable
+data class Game(val id: Long, val cover: Long, val first_release_date: Long, val genres: List<Long>, val name: String, val platforms: List<Long>, val summary: String, val total_rating: String)
