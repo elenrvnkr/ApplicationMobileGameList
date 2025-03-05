31415abc16afc9ee2c5e510869c39fd5666ea1db
Eléonore Vanacker <eleonore.vanacker@insa-rennes.fr>
2025-03-05 17:36:37 +0100

    tp3 et 4

--- app/src/main/java/com/insa/mygamelist/data/IGDBApi.kt
+++ app/src/main/java/com/insa/mygamelist/data/IGDBApi.kt
@@ -1,16 +1,28 @@
 package com.insa.mygamelist.data
 
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.Headers
+import retrofit2.http.POST
 
-object IGDBApi {
-    private const val BASE_URL = "https://api.igdb.com/v4/"
+interface IGDBApi {
 
-    val service: IGDBApiService by lazy {
-        Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(IGDBApiService::class.java)
-    }
+    @Headers("Accept: application/json")
+    @POST("games")
+    suspend fun getGames(@Body body: String): List<Game>
+
+    @Headers("Accept: application/json")
+    @POST("covers")
+    suspend fun getCovers(@Body body: String): List<Cover>
+
+    @Headers("Accept: application/json")
+    @POST("genres")
+    suspend fun getGenres(@Body body: String): List<Genre>
+
+    @Headers("Accept: application/json")
+    @POST("platforms")
+    suspend fun getPlatforms(@Body body: String): List<Platform>
+
+    @Headers("Accept: application/json")
+    @POST("platform_logos")
+    suspend fun getPlatformLogos(@Body body: String): List<PlatformLogo>
 }
